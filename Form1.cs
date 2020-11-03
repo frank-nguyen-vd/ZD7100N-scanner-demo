d656368e7309bf91bbf3e9f1dfc88b452a536837
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-11-03 13:46:10 +0800

    Open scanner given COM Port and Baud Rate

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scanner_demo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            int port = 0;
+            int bandrate = 0;
+            int hd = 0;
+            byte[] resp = new byte[64];
+            port = 17;
+            bandrate = 115200;
+            hd = Scan.OpenScaner(port, bandrate);            
+            if (hd > 0)
+            {                
+                MessageBox.Show("Connect to the device successfully", "Tips", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            // 
+            else
+                MessageBox.Show("Fail to connect to the device", "Tips", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ Scan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace scanner_demo
+{
+    class Scan
+    {
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "OpenScaner", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int OpenScaner(int port, int bandrate);
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "ScanStart", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ScanStart(int handle);
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "ScanClose", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ScanClose(int handle);
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "ScanDateReceiced", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]        
+        public static extern int ScanDateReceiced(int handle, ref byte buf);
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "VersionScaner", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int VersionScaner(int handle, ref byte buf);
+
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "GetSN", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetSN(int handle, ref byte buf);
+        
+        [DllImport("ZDSCANLIB.dll", EntryPoint = "CloseScaner", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CloseScaner(int handle);
+
+
+    }
+}
