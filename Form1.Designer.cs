1556054226a15a4fadea8f7687cd45943adca7fb
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-11-09 09:44:29 +0800

    Update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtClose = new System.Windows.Forms.Button();
             this.txtConnectionStatus = new System.Windows.Forms.Label();
+            this.btnStartScan = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStopScan = new System.Windows.Forms.Button();
+            this.btnRetrieveData = new System.Windows.Forms.Button();
+            this.txtScannedData = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -50,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 80);
+            this.label1.Location = new System.Drawing.Point(138, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 1;
@@ -58,7 +63,7 @@
             // 
             // txtComPort
             // 
-            this.txtComPort.Location = new System.Drawing.Point(92, 77);
+            this.txtComPort.Location = new System.Drawing.Point(205, 31);
             this.txtComPort.Name = "txtComPort";
             this.txtComPort.Size = new System.Drawing.Size(59, 20);
             this.txtComPort.TabIndex = 2;
@@ -67,7 +72,7 @@
             // 
             // txtBaudRate
             // 
-            this.txtBaudRate.Location = new System.Drawing.Point(92, 103);
+            this.txtBaudRate.Location = new System.Drawing.Point(205, 57);
             this.txtBaudRate.Name = "txtBaudRate";
             this.txtBaudRate.Size = new System.Drawing.Size(59, 20);
             this.txtBaudRate.TabIndex = 4;
@@ -77,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 106);
+            this.label2.Location = new System.Drawing.Point(138, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 3;
@@ -85,27 +90,79 @@
             // 
             // txtClose
             // 
-            this.txtClose.Location = new System.Drawing.Point(127, 31);
+            this.txtClose.Location = new System.Drawing.Point(186, 83);
             this.txtClose.Name = "txtClose";
             this.txtClose.Size = new System.Drawing.Size(78, 31);
             this.txtClose.TabIndex = 5;
             this.txtClose.Text = "Close";
             this.txtClose.UseVisualStyleBackColor = true;
-            this.txtClose.Click += new System.EventHandler(this.txtTurnOff_Click);
+            this.txtClose.Click += new System.EventHandler(this.txtClose_Click);
             // 
             // txtConnectionStatus
             // 
             this.txtConnectionStatus.AutoSize = true;
-            this.txtConnectionStatus.Location = new System.Drawing.Point(25, 9);
+            this.txtConnectionStatus.Location = new System.Drawing.Point(71, 9);
             this.txtConnectionStatus.Name = "txtConnectionStatus";
-            this.txtConnectionStatus.Size = new System.Drawing.Size(0, 13);
+            this.txtConnectionStatus.Size = new System.Drawing.Size(58, 13);
             this.txtConnectionStatus.TabIndex = 6;
+            this.txtConnectionStatus.Text = "Not Ready";
+            // 
+            // btnStartScan
+            // 
+            this.btnStartScan.Location = new System.Drawing.Point(28, 127);
+            this.btnStartScan.Name = "btnStartScan";
+            this.btnStartScan.Size = new System.Drawing.Size(78, 31);
+            this.btnStartScan.TabIndex = 7;
+            this.btnStartScan.Text = "Start Scan";
+            this.btnStartScan.UseVisualStyleBackColor = true;
+            this.btnStartScan.Click += new System.EventHandler(this.btnStartScan_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status:";
+            // 
+            // btnStopScan
+            // 
+            this.btnStopScan.Location = new System.Drawing.Point(186, 202);
+            this.btnStopScan.Name = "btnStopScan";
+            this.btnStopScan.Size = new System.Drawing.Size(78, 31);
+            this.btnStopScan.TabIndex = 9;
+            this.btnStopScan.Text = "Stop Scan";
+            this.btnStopScan.UseVisualStyleBackColor = true;
+            this.btnStopScan.Click += new System.EventHandler(this.btnStopScan_Click);
+            // 
+            // btnRetrieveData
+            // 
+            this.btnRetrieveData.Location = new System.Drawing.Point(28, 202);
+            this.btnRetrieveData.Name = "btnRetrieveData";
+            this.btnRetrieveData.Size = new System.Drawing.Size(78, 31);
+            this.btnRetrieveData.TabIndex = 10;
+            this.btnRetrieveData.Text = "Retrieve";
+            this.btnRetrieveData.UseVisualStyleBackColor = true;
+            this.btnRetrieveData.Click += new System.EventHandler(this.btnRetrieveData_Click);
+            // 
+            // txtScannedData
+            // 
+            this.txtScannedData.Location = new System.Drawing.Point(28, 176);
+            this.txtScannedData.Name = "txtScannedData";
+            this.txtScannedData.Size = new System.Drawing.Size(236, 20);
+            this.txtScannedData.TabIndex = 11;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(293, 263);
+            this.Controls.Add(this.txtScannedData);
+            this.Controls.Add(this.btnRetrieveData);
+            this.Controls.Add(this.btnStopScan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStartScan);
             this.Controls.Add(this.txtConnectionStatus);
             this.Controls.Add(this.txtClose);
             this.Controls.Add(this.txtBaudRate);
@@ -129,6 +186,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button txtClose;
         private System.Windows.Forms.Label txtConnectionStatus;
+        private System.Windows.Forms.Button btnStartScan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStopScan;
+        private System.Windows.Forms.Button btnRetrieveData;
+        private System.Windows.Forms.TextBox txtScannedData;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,7 +22,7 @@ namespace scanner_demo
             scanner = 0;
         }
 
-        private void showConnectionStatus(string msg, string severity)
+        private void showMessage(string msg, string severity)
         {
             txtConnectionStatus.Text = msg;
             switch (severity)
@@ -42,14 +42,14 @@ namespace scanner_demo
             }
         }
 
-        private void btnConnect_Click(object sender, EventArgs e)
+        private void connectScanner()
         {
             int port = 0;
-            int baudrate = 0;            
+            int baudrate = 0;
 
             if (scanner > 0)
             {
-                showConnectionStatus("Please turn off the connection first", "warning");
+                showMessage("Please turn off the connection first", "warning");
                 return;
             }
 
@@ -58,33 +58,102 @@ namespace scanner_demo
             scanner = Scan.OpenScaner(port, baudrate);
             if (scanner > 0)
             {
-                showConnectionStatus("Connect to device successfully", "info");
-            } else
-            {
-                showConnectionStatus("Fail to connect to device", "error");
+                showMessage("Connect to device successfully", "info");
             }
-                
+            else
+            {
+                showMessage("Fail to connect to device", "error");
+            }
         }
 
-        private void txtTurnOff_Click(object sender, EventArgs e)
+        private void closeScanner()
         {
             int status;
             status = Scan.CloseScaner(scanner);
             if (scanner == 0)
             {
-                showConnectionStatus("No active connection", "warning");
+                showMessage("No active connection", "warning");
                 return;
             }
 
             if (status != 0)
             {
-                showConnectionStatus("Device is turned off", "info");
+                showMessage("Device is turned off", "info");
                 scanner = 0;
-            } else
-            {
-                showConnectionStatus("Cannot turn off device", "error");
             }
-            
+            else
+            {
+                showMessage("Cannot turn off device", "error");
+            }
+        }
+
+        private void startScan()
+        {
+            int status = Scan.ScanStart(scanner);
+            if (status > 0)
+            {
+                showMessage("Start scanning", "info");
+            }
+            else
+            {
+                showMessage("Fail to start scanning", "error");
+            }
+        }
+
+        private void stopScan()
+        {
+            int status = Scan.ScanClose(scanner);
+            if (status > 0)
+            {
+                showMessage("Stop scanning", "info");
+            }
+            else
+            {
+                showMessage("Fail to stop scanning", "error");
+            }
+                
+        }
+
+        private void btnRetrieveData_Click(object sender, EventArgs e)
+        {
+            byte[] buf = new byte[1024];
+            int status;
+
+            status = 0;
+            txtScannedData.Text = "";
+                status = Scan.ScanDateReceiced(scanner, ref buf[0]);
+                if (status > 0)
+                {
+                    string res;
+                    res = System.Text.Encoding.Default.GetString(buf);
+                    if (res != txtScannedData.Text) txtScannedData.Text = res;
+                    showMessage("Data retrieved", "info");
+                }
+                else
+                {
+                    showMessage("Fail to retrieve data", "error");
+                }
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            connectScanner();
+        }
+
+        private void txtClose_Click(object sender, EventArgs e)
+        {
+            closeScanner();
+        }
+
+        private void btnStartScan_Click(object sender, EventArgs e)
+        {
+            startScan();
+        }
+
+        private void btnStopScan_Click(object sender, EventArgs e)
+        {
+            stopScan();
         }
     }
 }
